0f3ab3e8f8437f47f7d7cb30afa08ea075ccab60
sancshous <shura.starkov.2014@mail.ru>
2024-06-19 21:55:15 +0400

    Revert "Ver1.0.1"
    This reverts commit 53253567ac6f35ef9564a3c222349aff7781b639.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: 'dist'
+  server: {
+    proxy: {
+      '/lessons': 'http://localhost:3000'
+    }
   }
-});
+})
