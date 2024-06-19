53253567ac6f35ef9564a3c222349aff7781b639
sancshous <shura.starkov.2014@mail.ru>
2024-06-19 21:34:10 +0400

    Ver1.0.1

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/lessons': 'http://localhost:3000'
-    }
+  build: {
+    outDir: 'dist'
   }
-})
+});
