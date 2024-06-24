723838b2203161a0b8653c7181be6908f53ac459
Starkov <shura.starkov.2014@gmail.com>
2024-06-24 08:29:38 +0400

    v2.0.2

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import './assets/main.css'
-import { directoryImport } from 'directory-import'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const importedModules = directoryImport('./assets/images');
 const app = createApp(App)
 
 app.use(router)
