e16b9e15563001da57c51681d43b9f462a07e26c
Starkov <shura.starkov.2014@gmail.com>
2024-06-28 17:01:54 +0400

    v3.0.7

--- src/utils/importImages.js
+++ src/utils/importImages.js
@@ -1,6 +1,12 @@
 // src/utils/importImages.js
 
-// Используйте import.meta.glob для динамического импорта всех изображений
-const images = import.meta.globEager('@/assets/images/**/*.png');
+// Используем import.meta.glob для динамического импорта всех изображений
+const importAll = import.meta.glob('@/assets/images/**/*.png', { eager: true });
+
+let images = {};
+
+for (const path in importAll) {
+  images[path.replace('/assets/images/', '')] = importAll[path].default;
+}
 
 export default images;
